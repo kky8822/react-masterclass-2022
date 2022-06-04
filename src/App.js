9b6e08b472b0b54b27bad871bfe0b7924c2e8855
kky8822 <99176588+kky8822@users.noreply.github.com>
2022-06-04 23:22:54 +0900

    220604_Styled_Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+const Btn = styled.button`
+  background-color: tomato;
+  color: white;
+  border-radius: 15px;
+  height: 20px;
+  border: 0;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Box bgColor="tomato" />
+      <Circle bgColor="teal" />
+      <Btn>Hello</Btn>
+      <Btn as="a" href="/">
+        Hello link
+      </Btn>
+
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+    </Father>
   );
 }
 
